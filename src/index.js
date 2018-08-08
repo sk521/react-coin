c64caee642afdc9622ba752b368511e4131778ae
sk521 <samkwon521@gmail.com>
2018-08-08 15:53:18 -0400

    rendering react component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-alert("Hello World!")
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return <h1>React Coin</h1>;
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
