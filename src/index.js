a615787956d4100c37a88eea42c0c93b2e8974c6
sk521 <samkwon521@gmail.com>
2018-08-09 17:31:27 -0400

    hitting bit coin API and getting JSON res back

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/common/Header';
+import List from './components/list/List';
 import './index.css';
 
 const App = () => {
-  const title = 'React Coin';
+  // const title = 'React Coin';
   return (
     <div>
       <Header />
-      <h1>{title}</h1>
-      <p>Up to date crypto currencies financial data</p>
+
+      <List />
     </div>
   );
 }
