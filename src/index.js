efdc46237e4be2c98e0498f6ad8579cfc348de1a
sk521 <samkwon521@gmail.com>
2018-08-08 23:50:36 -0400

    Header fixed and css added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/common/Header';
+import './index.css';
 
 const App = () => {
   const title = 'React Coin';
