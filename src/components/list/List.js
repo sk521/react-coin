cef04a1ecf1fcbe3babd7edc438829a83acc0a38
sk521 <samkwon521@gmail.com>
2018-08-10 15:07:46 -0400

    fixed currency price and marketcap

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -79,14 +79,12 @@ class List extends React.Component {
                   {currency.name}
                 </td>
                 <td>
-                  <span className="Table-dollar">
-                    {currency.price}
-                  </span>
+                  <span className="Table-dollar">$</span>
+                  {currency.price}
                 </td>
                 <td>
-                  <span className="Table-dollar">
-                    {currency.marketCap}
-                  </span>
+                  <span className="Table-dollar">$</span>
+                  {currency.marketCap}
                 </td>
                 <td>
                   {this.renderChangePercentage(currency.percentChange24h)}
