f671ca2a444f181a685126e8ad7c40593685d833
sk521 <samkwon521@gmail.com>
2018-08-22 11:17:32 -0700

    detailed components added and link with img to route to home

--- src/components/detail/Detail.js
+++ src/components/detail/Detail.js
@@ -65,6 +65,20 @@ class Detail extends React.Component {
           <div className="Detail-item">
             24H Change <span className="Detail-value">{renderChangePercentage(currency.percentChange24h)}</span>
           </div>
+          <div className="Detail-item">
+            <span className="Detail-title">Market Cap</span>
+            <span className="Detail-dollar">$</span>
+            {currency.marketCap}
+          </div>
+          <div className="Detail-item">
+            <span className="Detail-title">24H Volume</span>
+            <span className="Detail-dollar">$</span>
+            {currency.volume24h}
+          </div>
+          <div className="Detail-item">
+            <span className="Detail-title">Total Supply</span>
+            {currency.totalSupply}
+          </div>
         </div>
       </div>
     )
