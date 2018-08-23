4df2e7f60ef7e3f4e53376102d5a42bd6281a8bd
sk521 <samkwon521@gmail.com>
2018-08-23 12:02:44 -0700

    search functionality hitting api

--- src/components/common/Search.js
+++ src/components/common/Search.js
@@ -1,12 +1,40 @@
 import React from 'react';
+import { API_URL } from '../../config';
+import { handleResponse } from '../../helper';
 import './Search.css';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchQuery: "",
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const inputValue = event.target.value;
+
+    this.setState({ searchQuery: inputValue });
+
+    // If searchQuery isn't present, dont send request to server
+    if (!inputValue) {
+      return '';
+    }
+
+    fetch(`${API_URL}/autocomplete?searchQuery=${inputValue}`)
+      .then(handleResponse)
+      .then((result) => {
+        console.log(result);
+      })
+  }
+
   render() {
     return (
       <form>
-        <input/>
-        <button>Submit</button>
+        <input onChange={this.handleChange} />
       </form>
     );
   }
