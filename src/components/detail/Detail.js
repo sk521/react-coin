dbce543925b170c7a5d02bf5990a77eb25374438
sk521 <samkwon521@gmail.com>
2018-08-24 16:29:00 -0400

    fixed detailed search bar

--- src/components/detail/Detail.js
+++ src/components/detail/Detail.js
@@ -18,6 +18,20 @@ class Detail extends React.Component {
   componentDidMount() {
     const currencyId = this.props.match.params.id;
 
+    this.fetchCurrency(currencyId);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.location.pathname !== nextProps.location.pathname) {
+
+      // Get new currency id from url
+      const newCurrencyId = nextProps.match.params.id;
+
+      this.fetchCurrency(newCurrencyId);
+    }
+  }
+
+  fetchCurrency(currencyId) {
     this.setState({ loading: true })
 
     fetch(`${API_URL}/cryptocurrencies/${currencyId}`)
